8163515d7e1cd1c6b7b37ab766e33aec40d3e2ce
vc743 <victorcontrerasvargas55@gmail.com>
2024-08-10 17:19:21 -0400

    Ejercicios de programacion condicional completos

---
+++ myFirstApp/programacion condicional/NivelPostulante/CalcularNivelDelPostulante.cs
@@ -0,0 +1,73 @@
+﻿namespace programacion_condicional.NivelPostulante
+{
+    internal class CalcularNivelDelPostulante
+    {
+        public void Calcular()
+        {
+            int totalPreguntas = 0;
+            int respuestasCorrectas = 0;
+            double porcentaje = 0;
+            string nivel;
+            string linea = string.Empty;
+
+            try
+            {
+                Console.WriteLine("Ingrese el numero total de preguntas:");
+                linea = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(linea))
+                {
+                    Console.WriteLine("El numero de preguntas es requerido.");
+                    return;
+                }
+
+                if (!int.TryParse(linea, out totalPreguntas))
+                {
+                    Console.WriteLine("El valor ingresado es invalido.");
+                    return;
+                }
+
+                Console.WriteLine("Ingrese el numero de repuestas correctas:");
+                linea = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(linea))
+                {
+                    Console.WriteLine("El numero de respuestas correctas es requerido.");
+                    return;
+                }
+
+                if (!int.TryParse(linea, out respuestasCorrectas))
+                {
+                    Console.WriteLine("El valor ingresado es invalido.");
+                    return;
+                }
+
+                porcentaje = (double)respuestasCorrectas / totalPreguntas * 100;
+
+                if (porcentaje >= 90)
+                {
+                    nivel = "Nivel maximo";
+                }
+                else if(porcentaje >= 75)
+                {
+                    nivel = "Nivel medio";
+                }
+                else if(porcentaje >= 50)
+                {
+                    nivel = "Nivel regular";
+                }
+                else
+                {
+                    nivel = "Fuera de nivel";
+                }
+
+                Console.WriteLine($"porcentaje: {porcentaje}");
+                Console.WriteLine($"El nivel del postulante es: {nivel}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ocurrio el siguiente error: {ex.Message}");
+            }
+        }
+    }
+}
--- myFirstApp/programacion condicional/Program.cs
+++ myFirstApp/programacion condicional/Program.cs
@@ -1,10 +1,15 @@
 ﻿//using programacion_condicional.SumayResta;
-using programacion_condicional.PromedioDeCalificaciones;
+//using programacion_condicional.PromedioDeCalificaciones;
+using programacion_condicional.NivelPostulante;
 
 //CalcularSumaYResta calcularSumaYResta = new CalcularSumaYResta();
 
 //calcularSumaYResta.Calcular();
 
-CalcularPromedioDeCalificaciones CalcularPromedioDeCalificaciones = new CalcularPromedioDeCalificaciones();
+//CalcularPromedioDeCalificaciones CalcularPromedioDeCalificaciones = new CalcularPromedioDeCalificaciones();
 
-CalcularPromedioDeCalificaciones.Calcular();
+//CalcularPromedioDeCalificaciones.Calcular();
+
+CalcularNivelDelPostulante calcularNivelDelPostulante = new CalcularNivelDelPostulante();
+
+calcularNivelDelPostulante.Calcular();

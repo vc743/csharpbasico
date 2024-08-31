f37686641cfc2e9ac067e955db0c3817bfea6f0e
vc743 <victorcontrerasvargas55@gmail.com>
2024-08-31 19:10:23 -0400

    agrega Ejemplo-paso-de-arreglos-a-metodos

--- myFirstApp/Ejemplo-de-arreglo3/obj/Debug/netcoreapp3.1/Ejemplo-de-arreglo3.AssemblyInfo.cs
+++ myFirstApp/Ejemplo-de-arreglo3/obj/Debug/netcoreapp3.1/Ejemplo-de-arreglo3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ejemplo-de-arreglo3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d3e17e8c6d73dd2a4721972e15014040afd82ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d2f99aa641d12061d96bc662733f766f08d0a47")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ejemplo-de-arreglo3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ejemplo-de-arreglo3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
